9fcbb75a56e243cd40b7e73b7a5f124ffc2e63c4
MarkLiebs <marklieberman@Marks-MacBook-Pro-2.local>
2016-05-07 17:02:47 -0400

    JS adds buttons correctly

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -1,70 +1,47 @@
-var movies = ['The Matrix', 'The Notebook', 'Mr. Nobody', 'The Lion King'];
+// Initial array of movies
+	var animals = ['Giraffe', 'Spider Monkey', 'Dog', 'Cat'];
 
-			// ========================================================
+	// ========================================================
 
-			// Generic function for capturing the movie name from the data-attribute
-			function alertMovieName() {
-				var movieName = $(this).data("name");
+	// Generic function for displaying movie data 
+	function renderButtons(){ 
 
-				alert(movieName);
+		// Deletes the movies prior to adding new movies (this is necessary otherwise you will have repeat buttons)
+		$('#gifView').empty();
 
-				// YOUR CODE GOES HERE!!!
+		// Loops through the array of movies
+		for (var i = 0; i < animals.length; i++){
 
-			}
+			// Then dynamicaly generates buttons for each movie in the array
 
-			// ========================================================
+			// Note the jQUery syntax here... 
+		    var a = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
+		    a.addClass('animal'); // Added a class 
+		    a.attr('data-name', animals[i]); // Added a data-attribute
+		    a.text(animals[i]); // Provided the initial button text
+		    $('#gifView').append(a); // Added the button to the HTML
+		}
+	}
 
-			// Generic function for displaying movie data 
-			function renderButtons() {
+	// ========================================================
 
-				// Deletes the movies prior to adding new movies (this is necessary otherwise you will have repeat buttons)
-				$('#moviesView').empty();
+	// This function handles events where one button is clicked
+	$('#addGif').on('click', function(){
 
-				// Loops through the array of movies
-				for (var i = 0; i < movies.length; i++) {
+		// This line of code will grab the input from the textbox
+		var animal = $('#gif-input').val().trim();
 
-					// Then dynamicaly generates buttons for each movie in the array
+		// The movie from the textbox is then added to our array
+		animals.push(animal);
+		
+		// Our array then runs which handles the processing of our movie array
+		renderButtons();
 
-					// Note the jQUery syntax here... 
-					var a = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
-					a.addClass('movie'); // Added a class 
-					a.attr('data-name', movies[i]); // Added a data-attribute
-					a.text(movies[i]); // Provided the initial button text
-					$('#moviesView').append(a); // Added the button to the HTML
-				}
-			}
+		// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
+		return false;
+	})
 
-			// ========================================================
+	// ========================================================
 
-			// This function handles events where one button is clicked
-			$('#addMovie').on('click', function () {
-
-				// This line of code will grab the input from the textbox
-				var movie = $('#movie-input').val().trim();
-
-				// The movie from the textbox is then added to our array
-				movies.push(movie);
-
-				// Our array then runs which handles the processing of our movie array
-				renderButtons();
-
-				// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
-				return false;
-			})
-
-			// ========================================================
-
-			// Generic function for displaying the movieInfo
-
-			// BAD CODE = won't work for new buttons (can't capture elements generated dynamically) 
-			// $('.movie').on('click', alertMovieName); 
-
-			// GOOD CODE = will work for both the original buttons and all of the new buttons
-			$(document).on('click', '.movie', alertMovieName);
-
-
-
-			// ========================================================
-
-			// This calls the renderButtons() function
-			renderButtons();
+	// This calls the renderButtons() function
+	renderButtons();

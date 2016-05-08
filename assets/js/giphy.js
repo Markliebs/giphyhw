0c8c5e57b7e9b199831d54270839bb1f6ab85831
MarkLiebs <marklieberman@Marks-MacBook-Pro-2.local>
2016-05-07 22:06:43 -0400

    Tweaking

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -1,89 +1,96 @@
 $(document).ready(function () {
 
-// Initial array of animals
-var animals = ['Giraffe', 'Elephant', 'Dog', 'Cat'];
+	// Initial array of animals
+	var animals = ['Giraffe', 'Elephant', 'Dog', 'Cat'];
 
-// ========================================================
+	// ========================================================
 
-// Generic function for displaying animal data 
-function renderButtons() {
+	// Generic function for displaying animal data 
+	function renderButtons() {
 
-	// Deletes the movies prior to adding new animals (this is necessary otherwise you will have repeat buttons)
-	$('#animalButtons').empty();
+		// Deletes the movies prior to adding new animals (this is necessary otherwise you will have repeat buttons)
+		$('#animalButtons').empty();
 
-	// Loops through the array of movies
-	for (var i = 0; i < animals.length; i++) {
+		// Loops through the array of movies
+		for (var i = 0; i < animals.length; i++) {
 
-		// Then dynamicaly generates buttons for each animal in the array
+			// Then dynamicaly generates buttons for each animal in the array
 
-		var a = $('<button>'); // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
-		a.addClass('animal'); // Added a class 
-		a.addClass('btn btn-primary btn-xs');
-		a.attr('data-name', animals[i]); // Added a data-attribute
-		a.text(animals[i]); // Provided the initial button text
-		$('#animalButtons').append(a); // Added the button to the HTML
+			var a = $('<button>'); // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
+			a.addClass('animal'); // Added a class 
+			a.addClass('btn btn-primary btn-xs');
+			a.attr('data-name', animals[i]); // Added a data-attribute
+			a.text(animals[i]); // Provided the initial button text
+			$('#animalButtons').append(a); // Added the button to the HTML
 
+		}
 	}
-}
 
-// ========================================================
+	// ========================================================
 
-// This function handles events where one button is clicked
-$('#addButton').on('click', function () {
+	// This function handles events where one button is clicked
+	$('#addButton').on('click', function () {
 
-	// This line of code will grab the input from the textbox
-	var animal = $('#gif-input').val().trim();
+		// This line of code will grab the input from the textbox
+		var animal = $('#gif-input').val().trim();
+		console.log("test");
 
-	// The animal from the textbox is then added to our array
-	animals.push(animal);
+		// The animal from the textbox is then added to our array
+		animals.push(animal);
 
-	// Our array then runs which handles the processing of our animal array
+		// Our array then runs which handles the processing of our animal array
+		renderButtons();
+
+		// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
+		return false;
+	});
+
+	// ========================================================
+
+	// This calls the renderButtons() function
 	renderButtons();
 
-	// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
-	return false;
-});
+	// The next section performs the search and returns the GIFs
 
-// ========================================================
+	$('button').on('click', function () {
+		var animal = $(this).data('name');
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-// This calls the renderButtons() function
-renderButtons();
+		$.ajax({
+			url: queryURL,
+			method: 'GET'
+		})
+			.done(function (response) {
 
-// The next section performs the search and returns the GIFs
+				console.log(queryURL);
 
-$('button').on('click', function () {
-	var animal = $(this).data('name');
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+				console.log(response);
 
-	$.ajax({
-		url: queryURL,
-		method: 'GET'
-	})
-		.done(function (response) {
+				var results = response.data;
+				$('#gifView').empty();
+				for (var i = 0; i < results.length; i++) {
 
-			console.log(queryURL);
+					var animalDiv = $('<div>');
 
-			console.log(response);
+					var p = $('<p>').text("Rating: " + results[i].rating);
 
-			var results = response.data;
-$('#gifView').empty();
-			for (var i = 0; i < results.length; i++) {
-				
-				
-				var animalDiv = $('<div>');
+					var animalImage = $('<img>');
+					animalImage.attr('src', results[i].images.fixed_height.url);
 
-				var p = $('<p>').text("Rating: " + results[i].rating);
+					animalDiv.prepend(p);
+					animalDiv.prepend(animalImage);
 
-				var animalImage = $('<img>');
-				animalImage.attr('src', results[i].images.fixed_height.url);
+					$('#gifView').prepend(animalDiv);
+					//--------------------------------
+				}
 
-				animalDiv.prepend(p);
-				animalDiv.prepend(animalImage);
-
-				$('#gifView').prepend(animalDiv);
-				//--------------------------------
-			}
-
-		});
-});
+			});
+	});
+	// The next section should allow for pausing and unpausing of the GIFs
+	
+	
+	
+	
+	
+	
 });
